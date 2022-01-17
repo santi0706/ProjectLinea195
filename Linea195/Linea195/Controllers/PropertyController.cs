00f35415b752c18192dfb1519165d6c7787fa7dc
santi0706 <47045229+santi0706@users.noreply.github.com>
2022-01-17 18:52:58 -0500

    Updated

--- Linea195/Linea195/Controllers/PropertyController.cs
+++ Linea195/Linea195/Controllers/PropertyController.cs
@@ -1,4 +1,5 @@
 ﻿using Linea195.Data.Repositories;
+using Linea195.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Linea195.Controllers
@@ -9,7 +10,7 @@ namespace Linea195.Controllers
     {
         private readonly IPropertyRepositories _propertyRepositories;
 
-        public PropertyController(IPropertyRepositories, propertyRepositories)
+        public PropertyController(IPropertyRepositories propertyRepositories)
         {
             _propertyRepositories = propertyRepositories;
         }
@@ -17,16 +18,55 @@ namespace Linea195.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllProperty()
         {
-            return Ok(await _propertyRepositories, GetProperty());
+            return Ok(await _propertyRepositories.GetProperty());
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            return Ok(await _propertyRepositories, InsertProperty(int id));
+            return Ok(await _propertyRepositories.InsertProperty(id));
         }
 
-        public async Task<IActionResult> InsertImage()
+        [HttpPost]
+        public async Task<IActionResult> InsertImage([FromBody] Propertys propertys)
         {
-
+            if (propertys == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var created = await _propertyRepositories.InsertImage(propertys);
+            return Created("imagen agregada", created);
         }
+        [HttpPost]
+        public async Task<IActionResult>UpdateProperty([FromBody] Propertys propertys)
+        {
+            if (propertys == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var created = await _propertyRepositories.UpdateProperty(propertys);
+            return Created("Propiedad alterada", created);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdatePrice([FromBody] Propertys propertys)
+        {
+            if (propertys == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var created = await _propertyRepositories.UpdatePrice(propertys);
+            return Created("Precio cambiado", created);
+        }
+
     }
